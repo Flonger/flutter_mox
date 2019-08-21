357a71483cbae26a2bfdf72da38828b6f90063a4
Flonger <xue@flonger.com>
2019-08-21 11:20:01 +0800

    no message

---
+++ demo/lib/counter.dart
@@ -0,0 +1,30 @@
+import 'package:mobx/mobx.dart';
+
+part 'counter.g.dart';
+
+class Counter = CounterBase with _$Counter;
+
+final Counter counter = Counter();
+
+abstract class CounterBase implements Store{
+  @observable
+  int value = 0;
+
+  @action
+  void increment(){
+    value ++;
+  }
+
+  @action
+  void decrement(){
+    value --;
+  }
+
+  @action
+  void set(int value){
+    this.value = value;
+  }
+
+  @computed
+  bool get isEven => value % 2==0;
+}
